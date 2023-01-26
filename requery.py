db071f4ae05ee747d4672a54ce3a50732bc97dc6
Stephan Lemmer <lemmersj@umich.edu>
2023-01-26 16:23:19 -0500

    Still fixing files

--- requery.py
+++ requery.py
@@ -3,6 +3,46 @@ import random
 import torch
 import numpy as np
 
+class QuasiRandomRequery():
+    def __init__(self, threshold, num_samples=50):
+        """The initializer.
+
+        args:
+            threshold: threshold for re-query. Since we're using uniform random, this is also the probability that the query is accepted.
+        """
+        self.threshold = threshold
+        self.num_samples = num_samples
+        self.count = 0
+        self.deferred = 0
+        self.required = float(threshold*num_samples)
+
+        if not self.required.is_integer():
+            print(f"Division is not even: {self.required}")
+
+    def should_requery(self, distribution):
+        """Should we re-query?
+
+        args:
+            distribution: the output distribution. Unused, but required for inheritance.
+
+        returns:
+            boolean. True for re-query, false for not.
+        """
+        random_val = random.random()
+        remaining_chances = self.num_samples-self.count
+        deferrals_remaining = float(self.required - self.deferred)
+        if deferrals_remaining == 0:
+            actual_p = 0
+        else:
+            actual_p = max(self.threshold, deferrals_remaining/remaining_chances)
+        print("---")
+        print(remaining_chances, deferrals_remaining, actual_p)
+        print("---")
+        if random_val < actual_p:
+            self.deferred += 1
+        self.count += 1
+        return random_val < actual_p
+
 class RandomRequery():
     def __init__(self, threshold):
         """The initializer.
